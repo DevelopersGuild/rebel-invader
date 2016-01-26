96327bd94339ae92affd51c45401e96725d3aad6
Thomas Tran <thomastran878@gmail.com>
2016-01-25 19:09:39 -0800

    Added several functions to HealthComponent and HealthSystem

--- core/src/com/developersguild/pewpew/components/HealthComponent.java
+++ core/src/com/developersguild/pewpew/components/HealthComponent.java
@@ -1,11 +1,16 @@
 package com.developersguild.pewpew.components;
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by Thomas on 1/22/2016.
  */
 public class HealthComponent implements Component {
-    public final int MAX_HEALTH = 100;
-    public static final int WIDTH = 10;
-    public static final int HEIGHT = 1;
+    public static final int STARTING_HEALTH = 100;
+    public static float healthMultiplier = 1.0f;
+    public int maxHealth;
     public int currentHealth;
+    public static final float LEVEL_INC = 0.1f;
+    public static final int WIDTH = 10;     // Temporary values, What are width and height for?
+    public static final int HEIGHT = 1;
 }
--- core/src/com/developersguild/pewpew/systems/HealthSystem.java
+++ core/src/com/developersguild/pewpew/systems/HealthSystem.java
@@ -1,26 +1,94 @@
 package com.developersguild.pewpew.systems;
 import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector2;
 import com.developersguild.pewpew.components.HealthComponent;
+import com.developersguild.pewpew.components.StructureComponent;
 import com.developersguild.pewpew.components.TransformComponent;
 /**
  * Created by Thomas on 1/23/2016.
  */
 public class HealthSystem extends IteratingSystem {
+    private static final Family family = Family.all(TransformComponent.class,
+            HealthComponent.class, StructureComponent.class).get();
+
     private ComponentMapper<TransformComponent> tm;
     private ComponentMapper<HealthComponent> hm;
+    private ComponentMapper<StructureComponent> rm;
+
+    private Engine engine;
+    private Vector2 tmp = new Vector2();
+
     public HealthSystem()
     {
         super(Family.all(TransformComponent.class, HealthComponent.class).get());
         tm = ComponentMapper.getFor(TransformComponent.class);
         hm = ComponentMapper.getFor(HealthComponent.class);
+        rm = ComponentMapper.getFor(StructureComponent.class);
+
+        // TODO: Set currentHealth in constructor
+        // currentHealth = STARTING_HEALTH * healthMultiplier;
     }
+
+    @Override
+    public void addedToEngine(Engine engine)
+    {
+        this.engine = engine;
+    }
+
     @Override
     public void processEntity(Entity entity, float deltaTime)
     {
+        StructureSystem structureSystem = engine.getSystem(StructureSystem.class);
+
         TransformComponent pos = tm.get(entity);
         HealthComponent health = hm.get(entity);
+        StructureComponent structure = rm.get(entity);
+
+        if (health.currentHealth == 0)
+        {
+            // TODO: Determine entity type and call the die method for specific entity
+            structureSystem.die(entity);    // For testing purposes
+        }
+
+        // TODO: Have healthBar follow entity
+    }
+
+    public void takeDamage(Entity entity, int damageValue)
+    {
+        if (!family.matches(entity)) return;
+
+        HealthComponent health = hm.get(entity);
+
+        health.currentHealth -= damageValue;
+
+        if (health.currentHealth < 0)
+        {
+            health.currentHealth = 0;
+        }
+    }
+
+    public void recoverHealth(Entity entity, int recoverValue)
+    {
+        if (!family.matches(entity)) return;
+
+        HealthComponent health = hm.get(entity);
+
+        health.currentHealth += recoverValue;
+
+        if (health.currentHealth > health.maxHealth)
+        {
+            health.currentHealth = health.maxHealth;
+        }
+    }
+
+    public void nextLevel(Entity entity)
+    {
+        HealthComponent health = hm.get(entity);
+
+        health.healthMultiplier += health.LEVEL_INC;
     }
 }
