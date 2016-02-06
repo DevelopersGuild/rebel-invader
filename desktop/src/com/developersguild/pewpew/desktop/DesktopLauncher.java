02db03250d6a20f05e317ea31a1536d5db960265
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-02-06 12:48:00 -0800

    More formatting (thanks JetBrains <3)

--- desktop/src/com/developersguild/pewpew/desktop/DesktopLauncher.java
+++ desktop/src/com/developersguild/pewpew/desktop/DesktopLauncher.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.developersguild.pewpew.PewPew;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Pew Pew";
-		config.width = 480;
-		config.height = 800;
-		new LwjglApplication(new PewPew(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Pew Pew";
+        config.width = 480;
+        config.height = 800;
+        new LwjglApplication(new PewPew(), config);
+    }
 }
--- html/src/com/developersguild/pewpew/client/HtmlLauncher.java
+++ html/src/com/developersguild/pewpew/client/HtmlLauncher.java
@@ -7,13 +7,13 @@ import com.developersguild.pewpew.PewPew;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 800);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        return new GwtApplicationConfiguration(480, 800);
+    }
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new PewPew();
-        }
+    @Override
+    public ApplicationListener getApplicationListener() {
+        return new PewPew();
+    }
 }
