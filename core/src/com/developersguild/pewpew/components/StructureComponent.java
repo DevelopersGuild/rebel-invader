6d809ea0ac99df2a26272743d79b89bc0a01018b
planetguy32 <apricefrench2d@gmail.com>
2016-02-09 19:23:53 -0800

    Shrank obstacles to 64x64

--- core/src/com/developersguild/pewpew/components/StructureComponent.java
+++ core/src/com/developersguild/pewpew/components/StructureComponent.java
@@ -8,8 +8,8 @@ import com.badlogic.ashley.core.Component;
 public class StructureComponent implements Component {
     // Change width and height appropriately once sprite is running
     public static final float STARTING_HEALTH = 100f;
-    public static final float WIDTH = 3.0f;
-    public static final float HEIGHT = 4.0f;
+    public static final float WIDTH = 2.0f;
+    public static final float HEIGHT = 2.0f;
     public static final float DAMAGE = 1f;
 
     public float maxHealth;
