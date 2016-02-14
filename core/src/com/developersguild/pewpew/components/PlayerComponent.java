db1240ef8fec40472c94f718fe90d06b213951ca
MW <mwmw4646@gmail.com>
2016-02-13 16:49:59 -0800

    Implemented fire rate

--- core/src/com/developersguild/pewpew/components/PlayerComponent.java
+++ core/src/com/developersguild/pewpew/components/PlayerComponent.java
@@ -9,11 +9,13 @@ public class PlayerComponent implements Component {
     public static final float STARTING_HEALTH = 100f;
     public static final int STATE_NORMAL = 0;
     public static final int STATE_KNOCKED_BACK=1;
+    public static final int BULLETS_PER_SECOND = 4;
     
     public static final float VELOCITY_X = 2500f;
     public static final float VELOCITY_Y = 50f;
     public static final float WIDTH = 2f;
     public static final float HEIGHT = 2f;
+    public static final float FIRE_RATE = 1.0f / BULLETS_PER_SECOND;
     
     public static final float KNOCKBACK_DURATION=0.4f;
 
@@ -22,4 +24,5 @@ public class PlayerComponent implements Component {
 
     public float heightSoFar = 0.0f;
     public float impactCooldown=0.0f;
+    public float shootTimer = 0.0f;
 }
