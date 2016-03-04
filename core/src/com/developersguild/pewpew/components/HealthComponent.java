a8f975d736cff4dab5846316b1391cd5085c1891
MW <mwmw4646@gmail.com>
2016-03-04 15:31:32 -0800

    Add boolean to Health to know if an entity is killed by Player

--- core/src/com/developersguild/pewpew/components/HealthComponent.java
+++ core/src/com/developersguild/pewpew/components/HealthComponent.java
@@ -1,7 +1,27 @@
 package com.developersguild.pewpew.components;
 
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.math.Vector3;
+
 /**
- * Created by MW on 04-Mar-16.
+ * Created by Thomas on 1/22/2016.
  */
-public class HealthComponent {
+public class HealthComponent implements Component {
+    public static final int IS_PLAYER = 1;
+    public static final int IS_ENEMY = 2;
+    public static final int IS_STRUCTURE = 3;
+    public static final float LEVEL_INC = 0.1f;     // The percent increase of health every level
+
+    public static float healthMultiplier = 1.0f;
+    public static int currentLvl = 1;
+    public float maxHealth;
+    public float currentHealth;
+    public Entity target;
+    public Vector3 targetPos;
+    public int belongsTo;
+    public float lengthRatio;
+    public float widthRatio;
+
+    public boolean doRender = true;
 }
