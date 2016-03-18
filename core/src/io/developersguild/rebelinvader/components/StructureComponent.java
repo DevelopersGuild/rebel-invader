96d5623009f8b4f0767691b00e1cd77d4b523fbb
MW <mwmw4646@gmail.com>
2016-03-17 19:10:58 -0700

    modify enemy and structure constants

--- core/src/io/developersguild/rebelinvader/components/StructureComponent.java
+++ core/src/io/developersguild/rebelinvader/components/StructureComponent.java
@@ -8,10 +8,10 @@ import com.badlogic.ashley.core.Entity;
  */
 public class StructureComponent implements Component {
     // Change width and height appropriately once sprite is running
-    public static final float STARTING_HEALTH = 15.0f;
+    public static final float STARTING_HEALTH = 25.0f;
     public static final float WIDTH = 1.0f;
     public static final float HEIGHT = 1.0f;
-    public static final float DAMAGE = 25f;
+    public static final float DAMAGE = 10f;
     public static final int STATE_ALIVE = 0;
     public static final int STATE_DEAD = 1;
     public static final int SCORE_VALUE = 5;
